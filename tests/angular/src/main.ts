9553765b0d6f84cf28459f7f06cd418a340f0bd6
Rob Dodson <robdodson@google.com>
2017-06-25 11:14:21 -0700

    Basic react test setup

--- tests/angular/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
--- tests/react-tests/src/root-test.js
+++ tests/react-tests/src/root-test.js
@@ -4,14 +4,31 @@ import ReactTestUtils from 'react-dom/test-utils';
 import expect from 'expect';
 import { Root } from './root';
 
+let app = null;
 describe('root', function() {
+
+  beforeEach(function() {
+    app = document.createElement('div');
+    document.body.appendChild(app);
+  });
+
+  afterEach(function() {
+    document.body.removeChild(app);
+    app = null;
+  });
+
   it('renders without problems', function() {
-    let root = ReactTestUtils.renderIntoDocument(<Root />);
+    let root = ReactDOM.render(<Root />, app);
     expect(root).toExist();
+  });
+
+  it('can display a Custom Element with children created during connectedCallback?', function() {
+    let root = ReactDOM.render(<Root />, app);
     let wc = ReactDOM.findDOMNode(root.refs.wc);
     expect(wc).toExist();
     let heading = wc.querySelector('h1');
     expect(heading).toExist();
     expect(heading.textContent).toEqual('Hello from x-foo');
   });
+
 });
