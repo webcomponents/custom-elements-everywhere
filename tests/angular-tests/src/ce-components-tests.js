fd8e2a749e35879d7a1a0562cedc0a137d8d988f
Rob Dodson <robdodson@google.com>
2017-07-20 18:56:31 -0700

    angular wip

--- tests/angular-tests/src/ce-components-tests.js
+++ tests/angular-tests/src/ce-components-tests.js
@@ -66,17 +66,96 @@ describe('with children', function() {
   it('can display a Custom Element with children created during connectedCallback and handle hiding and showing the element', function() {
     let fixture = TestBed.createComponent(ComponentWithDifferentViews);
     fixture.detectChanges();
+    let component = fixture.componentInstance;
     let root = fixture.debugElement.nativeElement;
     let wc = root.querySelector('#wc');
     expectHasChildren(wc);
-    fixture.toggle();
+    component.toggle();
     fixture.detectChanges();
     let dummy = root.querySelector('#dummy');
     expect(dummy).toExist();
     expect(dummy.textContent).toEqual('Dummy view');
-    fixture.toggle();
+    component.toggle();
     fixture.detectChanges();
     wc = root.querySelector('#wc');
     expectHasChildren(wc);
   });
 });
+
+describe('attributes and properties', function() {
+  it('will set boolean properties on a Custom Element that has already been defined and upgraded', function() {
+    let root = render(<ComponentWithProperties />, scratch);
+    let wc = root.querySelector('#wc');
+    expect(wc.bool).toBe(true);
+  });
+
+  // it('will set numeric properties on a Custom Element that has already been defined and upgraded', function() {
+  //   let root = render(<ComponentWithProperties />, scratch);
+  //   let wc = root.querySelector('#wc');
+  //   expect(wc.num).toEqual(42);
+  // });
+
+  // it('will set string properties on a Custom Element that has already been defined and upgraded', function() {
+  //   let root = render(<ComponentWithProperties />, scratch);
+  //   let wc = root.querySelector('#wc');
+  //   expect(wc.str).toEqual('Preact');
+  // });
+
+  // it('will set array properties on a Custom Element that has already been defined and upgraded', function() {
+  //   let root = render(<ComponentWithProperties />, scratch);
+  //   let wc = root.querySelector('#wc');
+  //   expect(wc.arr).toEqual(['P', 'r', 'e', 'a', 'c', 't']);
+  // });
+
+  // it('will set object properties on a Custom Element that has already been defined and upgraded', function() {
+  //   let root = render(<ComponentWithProperties />, scratch);
+  //   let wc = root.querySelector('#wc');
+  //   expect(wc.obj).toEqual({ org: 'developit', repo: 'preact' });
+  // });
+
+  // it('will set boolean attributes on a Custom Element that has not already been defined and upgraded', function() {
+  //   let root = render(<ComponentWithUnregistered />, scratch);
+  //   let wc = root.querySelector('#wc');
+  //   expect(wc.hasAttribute('bool')).toBe(true);
+  // });
+
+  // it('will set numeric attributes on a Custom Element that has not already been defined and upgraded', function() {
+  //   let root = render(<ComponentWithUnregistered />, scratch);
+  //   let wc = root.querySelector('#wc');
+  //   expect(wc.getAttribute('num')).toEqual('42');
+  // });
+
+  // it('will set string attributes on a Custom Element that has not already been defined and upgraded', function() {
+  //   let root = render(<ComponentWithUnregistered />, scratch);
+  //   let wc = root.querySelector('#wc');
+  //   expect(wc.getAttribute('str')).toEqual('Preact');
+  // });
+
+  // // Related:
+  // // https://github.com/developit/preact/issues/678
+  // // https://github.com/developit/preact/pull/511
+  // it('will set array properties on a Custom Element that has not already been defined and upgraded', function() {
+  //   let root = render(<ComponentWithUnregistered />, scratch);
+  //   let wc = root.querySelector('#wc');
+  //   expect(wc.arr).toEqual(['P', 'r', 'e', 'a', 'c', 't']);
+  // });
+
+  // it('will set object properties on a Custom Element that has not already been defined and upgraded', function() {
+  //   let root = render(<ComponentWithUnregistered />, scratch);
+  //   let wc = root.querySelector('#wc');
+  //   expect(wc.obj).toEqual({ org: 'developit', repo: 'preact' });
+  // });
+});
+
+// describe('events', function() {
+//   it('can listen to events from a Custom Element', function() {
+//     let root = render(<ComponentWithEvent />, scratch);
+//     let component = root._component;
+//     let wc = root.querySelector('#wc');
+//     let toggle = root.querySelector('#toggle');
+//     expect(toggle.textContent).toEqual('false');
+//     wc.click();
+//     component.forceUpdate();
+//     expect(toggle.textContent).toEqual('true');
+//   });
+// });
