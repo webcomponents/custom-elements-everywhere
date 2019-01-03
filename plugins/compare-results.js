a04743eac3d8aa7f037c948877c2c2607c47f001
Rob Dodson <robdodson@google.com>
2019-01-03 14:34:32 -0800

    wip

--- plugins/compare-results.js
+++ plugins/compare-results.js
@@ -1,38 +1,49 @@
-const fs = require("fs-extra");
 const { join } = require("path");
 
 module.exports = {
   name: "Compare Results",
   run: ({ beforePath, afterPath }) => {
-    const shouldFailPR = false;
-    const oldResults = require(join(
+
+    // When testing locally afterpath will be '.'
+    // which resolves to this plugins directory.
+    // Change it to point to the root of the project.
+    if (afterPath === '.') {
+      afterPath = join(__dirname, '..');
+    }
+
+    let shouldFailPR = false;
+    const oldResultsPath = join(
       beforePath,
       "libraries",
       "angular",
+      "results",
       "results.json"
-    ));
-    console.log('old success:', oldResults.summary.success);
+    );
+    const oldResults = require(oldResultsPath);
 
-    const newResults = require(join(
+    const newResultsPath = join(
       afterPath,
       "libraries",
       "angular",
+      "results",
       "results.json"
-    ));
-    console.log('new success:', newResults.summary.success);
+    );
+    const newResults = require(newResultsPath);
 
-    if (newResults.summary.success != oldResults.summary.success) {
-      console.log('Results are different. Failing PR!');
-      shouldFailPR = true;
+    let pLog;
+    let mdLog;
+    shouldFailPR = newResults.summary.success != oldResults.summary.success;
+    if (shouldFailPR) {
+      pLog = "Oh no, results were different!";
+      mdLog = `
+        #Oh no, results were different!
+        ## Angular
+        *Before*: ${oldResults.summary.success}
+        *After*: ${newResults.summary.success}
+      `;
+    } else {
+      pLog = mdLog = "✅ All checks passed!"
     }
-
-    const pLog = "Oh no, results were different!";
-    const mdLog = `
-      #Oh no, results were different!
-      ## Angular
-      *Before*: ${oldResults.summary.success}
-      *After*: ${newResults.summary.success}
-    `;
 
     return Promise.resolve({
       failPR: shouldFailPR,
