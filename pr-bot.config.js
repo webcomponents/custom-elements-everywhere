e137b16b93bd662349db42f79a4f9c54a8e89355
Rob Dodson <robdodson@google.com>
2019-01-02 18:14:33 -0800

    Speed up testing.

--- pr-bot.config.js
+++ pr-bot.config.js
@@ -4,7 +4,8 @@ module.exports = {
   botUsername: `ce-pr-bot`,
   repoDetails: {
     owner: "robdodson",
-    repo: "custom-elements-everywhere"
+    repo: "webcomponents/custom-elements-everywhere"
   },
+  buildCommand: "echo 'Hello World'",
   plugins: [require("./plugins/compare-results.js")]
 };
