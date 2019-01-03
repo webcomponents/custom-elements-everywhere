871186fbf48262b28c5f9499d3324797cef547b4
Rob Dodson <robdodson@google.com>
2019-01-02 18:26:34 -0800

    Fix owner/repo

--- plugins/compare-results.js
+++ plugins/compare-results.js
@@ -2,6 +2,7 @@ module.exports = {
   name: "Compare Results",
   run: ({beforePath, afterPath} = {}) => {
     return Promise.resolve({
+      failPR: false,
       prettyLog: "Hello from example plugin.",
       markdownLog: "## Hello from example plugin."
     });
--- pr-bot.config.js
+++ pr-bot.config.js
@@ -3,8 +3,8 @@ const prbot = require("pr-bot");
 module.exports = {
   botUsername: `ce-pr-bot`,
   repoDetails: {
-    owner: "robdodson",
-    repo: "webcomponents/custom-elements-everywhere"
+    owner: "webcomponents",
+    repo: "custom-elements-everywhere"
   },
   buildCommand: "echo 'Hello World'",
   plugins: [require("./plugins/compare-results.js")]
