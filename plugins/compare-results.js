c9e72a9428bbe858ed38410eedd6ddff21112e17
Rob Dodson <robdodson@google.com>
2019-01-02 18:24:08 -0800

    Don't mess with status?

--- plugins/compare-results.js
+++ plugins/compare-results.js
@@ -2,7 +2,6 @@ module.exports = {
   name: "Compare Results",
   run: ({beforePath, afterPath} = {}) => {
     return Promise.resolve({
-      failPR: false,
       prettyLog: "Hello from example plugin.",
       markdownLog: "## Hello from example plugin."
     });
