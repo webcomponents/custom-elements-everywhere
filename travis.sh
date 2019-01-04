465f2f72c3ce4b71cfe6b0ae8d13821292fa3594
Rob Dodson <robdodson@google.com>
2019-01-04 13:14:03 -0800

    Debugging travis.

--- travis.sh
+++ travis.sh
@@ -12,6 +12,8 @@ TARGET_BRANCH="gh-pages"
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
   echo "Skipping deploy; just doing a build."
   npm install -g pr-bot
+  pwd
+  ls -lahG
   pr-bot
   exit 0
 fi
