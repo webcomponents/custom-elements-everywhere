5c932358a3b4bd95b139dafa0bfc3d1f526297ff
Peter Burns <rictic@google.com>
2021-12-15 11:09:57 -0800

    Actually fix stencil. (#1120)

--- libraries/stencil/src/components.js
+++ libraries/stencil/src/components.js
@@ -2,15 +2,13 @@ import "ce-without-children";
 import "ce-with-children";
 import "ce-with-properties";
 import "ce-with-event";
-import {
-  ComponentWithChildren,
-  ComponentWithChildrenRerender,
-  ComponentWithDeclarativeEvent,
-  ComponentWithDifferentViews,
-  ComponentWithImperativeEvent,
-  ComponentWithProperties,
-  ComponentWithoutChildren
-} from "../dist/custom-elements/index";
+import { ComponentWithChildren } from "../dist/components/component-with-children.js";
+import { ComponentWithChildrenRerender } from "../dist/components/component-with-children-rerender.js";
+import { ComponentWithDeclarativeEvent } from "../dist/components/component-with-declarative-event.js";
+import { ComponentWithDifferentViews } from "../dist/components/component-with-different-views.js";
+import { ComponentWithImperativeEvent } from "../dist/components/component-with-imperative-event.js";
+import { ComponentWithProperties } from "../dist/components/component-with-properties.js";
+import { ComponentWithoutChildren } from "../dist/components/component-without-children.js";
 
 customElements.define("component-without-children", ComponentWithoutChildren);
 customElements.define("component-with-children", ComponentWithChildren);
