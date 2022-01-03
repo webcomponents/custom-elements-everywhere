b1d5392a9913294d1e3e6dfc32d05f3c150e6e7c
rictic <rictic@gmail.com>
2022-01-03 08:39:33 -0600

    Remove elm-stuff cache before testing
    Hoping this will prevent issues with testing elm in CI

--- index.js
+++ index.js
@@ -20,30 +20,7 @@ const opts = require("minimist")(process.argv.slice(2), {
  * A short term list of libraries that we don't test. If we can't
  * fix it in a reasonable period of time, we should just remove it entirely.
  */
-const knownBroken = [
-  /* 
-  Error:
-  +-------------------------------------------------------------------------------
-  |  Corrupt File: /home/runner/work/custom-elements-everywhere/custom-elements-everywhere/libraries/elm/elm-stuff/0.19.1/o.dat
-  |   Byte Offset: 331591
-  |       Message: not enough bytes
-  |
-  | Please report this to https://github.com/elm/compiler/issues
-  | Trying to continue anyway.
-  +-------------------------------------------------------------------------------
-  
-  -- CORRUPT CACHE ---------------------------------------------------------------
-  
-  It looks like some of the information cached in elm-stuff/ has been corrupted.
-  
-  Try deleting your elm-stuff/ directory to get unstuck.
-  
-  Note: This almost certainly means that a 3rd party tool (or editor plugin) is
-  causing problems your the elm-stuff/ directory. Try disabling 3rd party tools
-  one by one until you figure out which it is!
-  */
-  'elm'
-];
+const knownBroken = [];
 
 /**
  * Build the list of directories to exclude from testing.
