339c791b8028c428da545430f2401d44c8230180
Justin Fagnani <justinfagnani@google.com>
2023-10-17 08:00:53 -0700

    Add hyperHTML test

--- libraries/hyperhtml/src/advanced-tests.js
+++ libraries/hyperhtml/src/advanced-tests.js
@@ -62,6 +62,14 @@ describe("advanced support", function() {
       let wc = root.querySelector("#wc");
       expect(wc.obj).to.eql({ org: "viperHTML", repo: "hyperHTML" });
     });
+
+    it("will pass object data to a camelCase-named property", async function() {
+      this.weight = 2;
+      ComponentWithProperties(root);
+      let wc = root.querySelector("#wc");
+      expect(wc.camelCaseObj).to.eql({ label: "passed" });
+    });
+
   });
 
   describe("events", function() {
