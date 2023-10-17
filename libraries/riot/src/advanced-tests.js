c9cd5338c67caba76d65f047360bd05925a22bbe
Justin Fagnani <justinfagnani@google.com>
2023-10-17 09:08:10 -0700

    Add Riot test

--- libraries/riot/src/advanced-tests.js
+++ libraries/riot/src/advanced-tests.js
@@ -57,6 +57,15 @@ describe('advanced support', function() {
       let data = wc.obj
       expect(data).to.eql({ org: 'riotjs', repo: 'riot' })
     })
+
+    it('will pass object data to a camelCase-named property', function() {
+      this.weight = 2
+      component(ComponentWithProperties)(scratch)
+      let wc = scratch.querySelector('#wc')
+      let data = wc.camelCaseObj;
+      expect(data).to.eql({ label: "passed" });
+    })
+
   })
 
   describe('events', function() {
