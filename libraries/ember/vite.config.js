9bc9d60af7effed4f7f024989361a8c01a15a4eb
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-03-05 19:19:58 -0500

    All tests pass

--- libraries/ember/vite.config.js
+++ libraries/ember/vite.config.js
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite'
-import { extensions, ember } from '@embroider/vite'
-import { babel } from '@rollup/plugin-babel'
+import { defineConfig } from 'vite';
+import { extensions, ember } from '@embroider/vite';
+import { babel } from '@rollup/plugin-babel';
+
+const validator = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/validator/index.js`;
+const tracking = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/tracking/index.js`;
+const eUtil = `${process.cwd()}/node_modules/@embroider/util/addon/index.js`;
+const cache = `${process.cwd()}/node_modules/ember-source/dist/packages/@glimmer/tracking/primitives/cache.js`;
 
 export default defineConfig({
+  build: {
+    target: ['esnext'],
+  },
+  resolve: {
+    extensions,
+    alias: {
+      '@glimmer/validator': validator,
+      '@glimmer/tracking/primitives/cache': cache,
+      '@glimmer/tracking': tracking,
+      '@embroider/util': eUtil,
+    },
+  },
   plugins: [
     ember(),
     babel({
@@ -10,4 +27,4 @@ export default defineConfig({
       extensions,
     }),
   ],
-})
+});
